7f3207a8263eaf52f455ac44aec7b9e0ace5374f
bharath-ravindranath <bharathr.26@gmail.com>
2016-07-31 16:19:00 -0400

    adding db

--- welcome.py
+++ welcome.py
@@ -14,40 +14,78 @@
 
 import os
 from flask import Flask, jsonify
-# import ibm_db
-# import ibmiotf.application
+import ibm_db
+import ibmiotf.application
+import json
 
+options = {
 
+}
 
 app = Flask(__name__)
 
-# @app.route('/')
-# def Welcome():
-#     return app.send_static_file('index.html')
+@app.route('/')
+def Welcome():
+  return app.send_static_file('index.html')
 
-@app.route('/myapp')
-def WelcomeToMyapp():
-    return 'Welcome again to my app running on Bluemix!'
+# @app.route('/myapp')
+# def WelcomeToMyapp():
+#     return 'Welcome again to my app running on Bluemix!'
 
-@app.route('/testing')
-def testnig():
-    return "Hi there!"
+# @app.route('/testing')
+# def testnig():
+#     return "Hi there!"
 
-@app.route('/api/people')
-def GetPeople():
-    list = [
-        {'name': 'John', 'age': 28},
-        {'name': 'Bill', 'val': 26}
-    ]
-    return jsonify(results=list)
+# @app.route('/api/people')
+# def GetPeople():
+#     list = [
+#         {'name': 'John', 'age': 28},
+#         {'name': 'Bill', 'val': 26}
+#     ]
+#     return jsonify(results=list)
 
-@app.route('/api/people/<name>')
-def SayHello(name):
-    message = {
-        'message': 'Hello ' + name
-    }
-    return jsonify(results=message)
+# @app.route('/api/people/<name>')
+# def SayHello(name):
+#     message = {
+#         'message': 'Hello ' + name
+#     }
+#     return jsonify(results=message)
+
+
+def myDeviceEventCallback(event):
+  s = json.dumps(event.data)
+  print(s)
+  s = json.loads(s)
+  print(s["d"])
+
+  if db2conn:
+    if s["d"]["topic"][:4] == "CSIE":
+      ins = ibm_db.exec_immediate(db2conn, "INSERT INTO DASH<>.CSIE (TIME_STAMP, SIGNAL_SENT) VALUES" + \
+       "('%s','%s')" %(s['d']['timestamp'], s['d']['signal']))
+    elif s['d']['topic'][:3] == 'MDV':
+      ins = ibm_db.exec_immediate(db2conn, "INSERT INTO DASH<>.MDV (ID, TIME_STAMP, SINGAL_RECEIVED, SPEED)"\
+       + "VALUES (%s, '%s', '%s', %s)" %(s['d']['id'],s['d']['tiemstamp'],s['d']['signal'], s['d']['speed']))
+
+
+def main():
+  try:
+    client = ibmiotf.application.Client(options)
+    client.deviceEventCallback = myDeviceEventCallback
+    client.subscribeToDeviceEvents(event="status")
+  except ibmiotf.ConnectionException as e:
+    print(e.value)
+
+  if 'VCAP_SERVICES' in os.environ:
+    db2info = json.loads(os.environ['VCAP_SERVICES'])['dashDB'][0]
+    db2cred = db2info['credentials']
+
+  db2conn = ibm_db.connect('DATABASE=' + db2cred['db'] + '; HOSTNAME=' + db2cred['hostname'] + '; PORT=' + \
+    db2cred['port'] + ';UID=' + db2cred['username'] + ';PWD=' + db2cred['password'] + ';', '', '')
+
+  global db2conn
+  app.run(host='0.0.0.0', port=int(port))
+
 
 port = os.getenv('PORT', '5000')
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=int(port))
+  main()
